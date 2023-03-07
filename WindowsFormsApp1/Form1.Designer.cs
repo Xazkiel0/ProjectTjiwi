3fce7279a80b32bb6f9cb0f3e83b59b976691c9a
Gweh <Gweh@DESKTOP-U7UBLL1>
2023-03-07 22:23:10 +0700

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(55D, 60D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(80D, 30D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(900D, 6D);
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Maximum = 100D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 100D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            series1.Points.Add(dataPoint1);
+            series1.Points.Add(dataPoint2);
+            series1.Points.Add(dataPoint3);
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(530, 352);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            this.chart1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseClick);
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ChartMoveCursor);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y});
+            this.dataGridView1.Location = new System.Drawing.Point(548, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            this.dataGridView1.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.dataGridView1_RowsRemoved);
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            this.dataGridView1.UserDeletedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // X
+            // 
+            dataGridViewCellStyle1.Format = "N2";
+            dataGridViewCellStyle1.NullValue = null;
+            this.X.DefaultCellStyle = dataGridViewCellStyle1;
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            dataGridViewCellStyle2.Format = "N2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.Y.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkUserRow();
+        }
+
+        private void chart1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (dataGridView1.AllowUserToAddRows != false)
+            {
+                double[] xy = getXYPos(e);
+                int idxClick = dataGridView1.Rows.Add(new object[]{ Math.Round(xy[0],2), Math.Round(xy[1],2) });
+                
+                checkUserRow();
+                checkCell(idxClick);
+            }
+        }
+
+        RectangleF ChartAreaClientRectangle(Chart chart, ChartArea CA)
+        {
+            RectangleF CAR = CA.Position.ToRectangleF();
+            float pw = chart.ClientSize.Width / 100f;
+            float ph = chart.ClientSize.Height / 100f;
+            return new RectangleF(pw * CAR.X, ph * CAR.Y, pw * CAR.Width, ph * CAR.Height);
+        }
+
+        RectangleF InnerPlotPositionClientRectangle(Chart chart, ChartArea CA)
+        {
+            RectangleF IPP = CA.InnerPlotPosition.ToRectangleF();
+            RectangleF CArp = ChartAreaClientRectangle(chart, CA);
+
+            float pw = CArp.Width / 100f;
+            float ph = CArp.Height / 100f;
+
+            return new RectangleF(CArp.X + pw * IPP.X, CArp.Y + ph * IPP.Y,
+                                    pw * IPP.Width, ph * IPP.Height);
+        }
+
+        ToolTip tt = null;
+        Point tl = Point.Empty;
+
+        private double[] getXYPos(MouseEventArgs e)
+        {
+            ChartArea ca = chart1.ChartAreas[0];
+
+            Axis ax = ca.AxisX;
+            Axis ay = ca.AxisY;
+            double x = ax.PixelPositionToValue(e.X);
+            double y = ay.PixelPositionToValue(e.Y);
+            return new double[]{x,y};
+        }
+        private void ChartMoveCursor(object sender, MouseEventArgs e)
+        {
+
+            if (tt == null) tt = new ToolTip();
+
+            ChartArea ca = chart1.ChartAreas[0];
+
+
+            if (InnerPlotPositionClientRectangle(chart1, ca).Contains(e.Location))
+            {
+                double[] pos = getXYPos(e);
+                if (e.Location != tl)
+                    tt.SetToolTip(chart1, string.Format("X={0:0.000} ; Y={1:0.00}", pos[0], pos[1]));
+
+                //tl = e.Location;
+            }
+            else tt.Hide(chart1);
+        }
+
+        private void bindData2Chart()
+        {
+            // Set the X and Y values for the chart
+            chart1.Series["Series1"].Points.DataBindXY(dataGridView1.Rows.Cast<DataGridViewRow>().Where(r => !r.Cells.Cast<DataGridViewCell>().All(c => c.Value == null)).Select(r => Convert.ToDouble(r.Cells[0].Value)).ToList(),
+                                                         dataGridView1.Rows.Cast<DataGridViewRow>().Where(r => !r.Cells.Cast<DataGridViewCell>().All(c => c.Value == null)).Select(r => Convert.ToDouble(r.Cells[1].Value)).ToList());
+            
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            bindData2Chart();
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            checkUserRow();
+        }
+
+        private void checkUserRow()
+        {
+            if (dataGridView1.Rows.Count > 5)
+                dataGridView1.AllowUserToAddRows = false;
+            else
+                dataGridView1.AllowUserToAddRows = true;
+        }
+
+        private void dataGridView1_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            checkUserRow();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) // Check if the changed cell belongs to a data row (not the header row)
+            {
+                // Get the values of the changed row
+                bindData2Chart();
+                checkCell(e.RowIndex);
+            }
+        }
+        private void checkCell(int rowIndex)
+        {
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+            DataGridViewCell cell = row.Cells[0];
+
+            // Get the values of the previous and next cells in the same column
+            object prevValue = rowIndex > 0 ? dataGridView1.Rows[rowIndex - 1].Cells[0].Value : null;
+            object nextValue = (rowIndex < dataGridView1.Rows.Count - 1) ? dataGridView1.Rows[rowIndex + 1].Cells[0].Value : null;
+            double dprevVal = prevValue == null ? double.NaN : Convert.ToDouble(prevValue);
+            double dnextVal = nextValue == null ? double.NaN : Convert.ToDouble(nextValue);
+
+            if (dprevVal.Equals(double.NaN) && rowIndex != 0)
+            {
+                MessageBox.Show("Fill any blank X values");
+                return;
+            }
+
+            double currVal;
+            if (double.TryParse(cell.Value.ToString(), out currVal))
+            {
+                if (currVal <= dprevVal)
+                {
+                    MessageBox.Show("The Value must be higher then previous value");
+                    dataGridView1.Rows.RemoveAt(rowIndex);
+                    // The value is between the values of the previous and next cells, do something...
+                }
+                else if (currVal >= dnextVal || dnextVal.Equals(double.NaN) && nextValue != null)
+                {
+                    MessageBox.Show("The Value must be lower then next value");
+                    dataGridView1.Rows.RemoveAt(rowIndex);
+
+                }
+            }
+            bindData2Chart();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
